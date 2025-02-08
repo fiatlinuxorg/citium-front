469006d0fbec84d1e16635e0c946554bd31ec52a
EtanoloYT <etanoloyt@gmail.com>
2025-02-08 17:28:27 +0100

    FRONT-13 Add dashboard route

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { Router, Route } from "svelte-routing";
   import Register from "./routes/Register.svelte";
   import Login from "./routes/Login.svelte";
+  import Dashboard from "./routes/Dashboard.svelte";
 
   export let url = "";
 </script>
@@ -9,4 +10,5 @@
 <Router {url}>
   <Route path="/register" component={Register} />
   <Route path="/login" component={Login} />
+  <Route path="/dashboard" component={Dashboard} />
 </Router>
