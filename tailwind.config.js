44440749961a1f817d587ac16ab1accc8f7f5309
EtanoloYT <etanoloyt@gmail.com>
2025-02-08 17:16:22 +0100

    FRONT-2 Set tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   plugins: [],
     theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EA2B1F',
+        light: '#F5F3F7',
+        dark: '#313638',
+        gray: '#DAD5D5',
+      },
+      fontFamily: {
+        body: ['Inter'],
+        display: ['Afacad'],
+      },
+      borderWidth: {
+        6: '6px'
+      }
+    },
   },
   content: ["./index.html",'./src/**/*.{svelte,js,ts}'], // for unused CSS
   variants: {
