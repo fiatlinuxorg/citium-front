a64244e8b554c5b1d125332ff7f4e6cce106b6e6
EtanoloYT <etanoloyt@gmail.com>
2025-02-11 15:01:25 +0100

    FRONT-17 Add new icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,21 @@ export default {
       },
       borderWidth: {
         6: '6px'
+      },
+      animation: {
+        'shake': 'shake 0.5s ease-in-out',
+        'slide-in-right': 'slide-in-right 0.5s ease-in-out',
+      },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'rotate(0)' },
+          '20%, 60%': { transform: 'rotate(-5deg)' },
+          '40%, 80%': { transform: 'rotate(5deg)' },
+        },
+        'slide-in-right': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
       }
     },
   },
